206500fbfb527c92d44e74a8cf3f644ed017270e
Johnson Chen <johnsonchen12345@gmail.com>
2017-07-07 19:24:02 +0000

    hi

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-// JavaScript File
-// JavaScript File
 $( document ).ready(function() {
+$("submit").click(function () {
+    var answer = $("input").val();
+    
+    if (answer==="yellow") {
+    alert("Correct"); 
+    }
+    
+    else {
+    alert("Wrong");
+    }
+    
+$("#cat").click(function () {
+  $("#cat").hide();  
+    for (var i = 0; i < 101; i = i +1) {
+        $("body").append("<img src=https://cdn-webimages.wimages.net/051823d5bcf3a040540832900c09c23bd2e679-wm.jpg?v=3>");
+    }
+    
+});
 
-
+        
+});
 });
