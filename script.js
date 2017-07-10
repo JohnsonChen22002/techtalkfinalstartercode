d58cebe4f639128543fa0586b07dab12da4f51e2
Johnson Chen <johnsonchen12345@gmail.com>
2017-07-10 16:00:22 +0000

    PersonalWebpage

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
-$("submit").click(function () {
+  
+  $("#cat").click(function () {
+  $("#cat").hide();  
+    for (var i = 0; i < 101; i = i +1) {
+        $("body").append("<img src=https://cdn-webimages.wimages.net/051823d5bcf3a040540832900c09c23bd2e679-wm.jpg?v=3>");
+    }
+    
+});
+
+$("#submit").click(function () {
     var answer = $("input").val();
     
     if (answer==="yellow") {
@@ -9,15 +18,6 @@ $("submit").click(function () {
     else {
     alert("Wrong");
     }
-    
-$("#cat").click(function () {
-  $("#cat").hide();  
-    for (var i = 0; i < 101; i = i +1) {
-        $("body").append("<img src=https://cdn-webimages.wimages.net/051823d5bcf3a040540832900c09c23bd2e679-wm.jpg?v=3>");
-    }
-    
-});
-
         
 });
 });
